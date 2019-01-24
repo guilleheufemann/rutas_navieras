09b7f8f666584a429673b57e3c5409023e4d7550
guilleheufemann <guillermo.heufemann@gmail.com>
2019-01-23 23:25:09 -0300

    Update flightanimation.js

--- js/flightanimation.js
+++ js/flightanimation.js
@@ -150,8 +150,8 @@ $(function() {
     }, 150);
   }
 
-  queue().defer(d3.json, "https://github.com/guilleheufemann/route_vessel/blob/master/countries2.topo.json")
-         .defer(d3.json, "https://github.com/guilleheufemann/route_vessel/blob/master/airports2.topo.json")
+  queue().defer(d3.json, "https://guilleheufemann.github.io/rutas_navieras/json/countries2.topo.json")
+         .defer(d3.json, "https://guilleheufemann.github.io/rutas_navieras/json/airports2.topo.json")
          .await(loaded);
 
   $(window).resize(function() {
